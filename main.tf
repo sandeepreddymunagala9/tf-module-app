40510d263c601bd6658b88069f525ee1c564d44f
Sandeep Reddy Munagala <samg22@student.bth.se>
2024-10-27 23:30:18 +0100

    creating files

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_security_group" "sg" {
 }
 ## ec2
 resource "aws_instance" "instance" {
-  ami = data.aws_ami.example.id
+  ami = data.aws_ami.ami.id
   instance_type = "t3.micro"
   security_groups = [aws_security_group.sg.id]
   iam_instance_profile = aws_iam_instance_profile.instance_profile.name
